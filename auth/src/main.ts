bb179690e5bd46fb3ae062c84dde3f16d36c17da
lukasz <bazyl603@gmail.com>
2021-11-27 14:11:21 +0100

    add env example

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  await app.listen(PORT);
   console.log('[START] app started');
 }
 
